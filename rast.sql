1819c8ba0cce3d57cf8d352d9eb345e3b6c2f4de
Eric Potash <eric@k2co3.net>
2017-02-28 00:45:27 +0000

    working

--- rast.sql
+++ rast.sql
@@ -1,16 +1,13 @@
 drop table if exists rast;
 create table rast as (
 
-with extent as (select st_extent(geom) as geom from city)
-
 select 1 as rid,
     st_addband(st_MakeEmptyRaster(
-        1024,1024,
---      st_x(st_centroid(geom)), st_y(st_centroid(geom)),
+        ${WIDTH},${HEIGHT},
         st_xmin(geom), st_ymax(geom),
-        (st_xmax(geom) - st_xmin(geom))/1024,
-        (st_ymin(geom) - st_ymax(geom))/1024,
-        0,0,4326), 
+        (st_xmax(geom) - st_xmin(geom))/${WIDTH},
+        (st_ymin(geom) - st_ymax(geom))/${HEIGHT},
+        0,0,${SRID}), 
         ARRAY[
             -- distance raster band 32 bit float
             ROW(1, '32BF'::text, -1, -1) 
