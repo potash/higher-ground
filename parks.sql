53a16ee2a8920a8ce490fa7b9c36caf471b034d2
Eric Potash <eric@k2co3.net>
2017-03-01 02:10:09 +0000

    add park conditions

--- parks.sql
+++ parks.sql
@@ -3,8 +3,10 @@ drop table if exists parks;
 create table parks as (
     select *
     from planet_osm_polygon
-    where leisure = 'park'
-    or "natural" is not null
+    where 
+        leisure in ('park','nature_preserve') or
+        landuse in ('conservation', 'forest') or
+        ("natural" is not null and "natural" != 'water')
 );
 
 create index parks_index on parks using gist (way);
